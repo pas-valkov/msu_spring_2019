28043b8afd2c738e117ca6731df12c07e82ee67b
pas-valkov <pas-valkov@yandex.ru>
2019-04-25 02:52:13 +0300

    Create pingpong.cpp

---
+++ 06/pingpong.cpp
@@ -0,0 +1,40 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <condition_variable>
+
+const size_t N = 1000000;
+size_t counter = 0;
+bool condition = false;
+std::mutex mutex;
+std::condition_variable ready;
+
+void ping() {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (counter < N-1) {
+        ready.wait(lock, []{return !condition;});
+        ++counter;
+        condition = !condition;
+        std::cout << "ping\n";
+        ready.notify_one();
+    }
+}
+
+void pong() {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (counter < N)    {
+        ready.wait(lock, []{return condition;});
+        ++counter;
+        condition = !condition;
+        std::cout << "pong\n";
+        ready.notify_one();
+    }
+}
+
+int main() {
+    std::thread t1(ping);
+    std::thread t2(pong);
+    t1.join();
+    t2.join();
+    return 0;
+}
